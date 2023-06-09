3f3a8a1f6e6028327e4e99f6433bdcebbfd8a8c3
chandirasegaran <chandirasegaransegar@gmail.com>
2023-06-09 15:46:16 +0530

    Added UI for CR Login Page Added UI for Student Login Page

--- lib/crlogin.dart
+++ lib/crlogin.dart
@@ -11,61 +11,66 @@ class Crlogin extends StatefulWidget {
 class _CrloginState extends State<Crlogin> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('CR Login'),
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+    return Theme(
+      data: ThemeData(
+        useMaterial3: true,
       ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10.0),
-                color: Colors.white,
-              ),
-              child: TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Register Number',
-                  prefixIcon: Icon(Icons.person),
-                  border: InputBorder.none,
-                ),
-              ),
-            ),
-            SizedBox(height: 20),
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10.0),
-                color: Colors.white,
-              ),
-              child: TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  prefixIcon: Icon(Icons.lock),
-                  border: InputBorder.none,
-                ),
-                obscureText: true,
-              ),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Implement the login logic here
-              },
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.symmetric(horizontal: 40, vertical: 16),
-                shape: RoundedRectangleBorder(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('CR Login'),
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(10.0),
+                  color: Colors.white,
+                ),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Register Number',
+                    prefixIcon: Icon(Icons.person),
+                    border: InputBorder.none,
+                  ),
                 ),
               ),
-              child: Text(
-                'Login',
-                style: TextStyle(fontSize: 18),
+              SizedBox(height: 20),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  color: Colors.white,
+                ),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    prefixIcon: Icon(Icons.lock),
+                    border: InputBorder.none,
+                  ),
+                  obscureText: true,
+                ),
               ),
-            ),
-          ],
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  // Implement the login logic here
+                },
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.symmetric(horizontal: 40, vertical: 16),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+                child: Text(
+                  'Login',
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/studlogin.dart
+++ lib/studlogin.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:pu_attendance/main.dart';
 
 class StudentLoginPage extends StatefulWidget {
-  const StudentLoginPage({super.key});
+  const StudentLoginPage({Key? key}) : super(key: key);
 
   @override
   State<StudentLoginPage> createState() => _StudentLoginPageState();
@@ -16,10 +16,77 @@ class _StudentLoginPageState extends State<StudentLoginPage> {
         title: Text('Student Login'),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
-      body: Center(
-        child: Text('Student Login Page'),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.account_circle,
+              size: 80,
+              color: Theme.of(context).colorScheme.onBackground,
+            ),
+            SizedBox(height: 20),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10.0),
+                color: Colors.white,
+              ),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Register Number',
+                  prefixIcon: Icon(Icons.person),
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10.0),
+                color: Colors.white,
+              ),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  prefixIcon: Icon(Icons.lock),
+                  border: InputBorder.none,
+                ),
+                obscureText: true,
+              ),
+            ),
+            SizedBox(height: 20),
+            TextButton(
+              onPressed: () {
+                // Implement the action for the 'Forgot Password' button here
+              },
+              child: Text(
+                'Forgot Password',
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Implement the login logic here
+              },
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.symmetric(horizontal: 40, vertical: 16),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+              child: Text(
+                'Login',
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+          ],
+        ),
       ),
     );
-    ;
   }
 }
